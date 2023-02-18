c1c81712d4a0488f8501682694671831a32bdcf4
Hakkija <Henri.tallinn@gmail.com>
2023-02-18 21:31:03 +0200

    random updated

---
+++ random/got_brain.py
@@ -0,0 +1,7 @@
+user_name = input("Enter name: ")
+got_brain = input("Do you have brain y/n: ")
+if got_brain.lower() == "y":
+    print(f"Congratulations {user_name} you have brain!!")
+else:
+    print(f"Please go study {user_name}!")
+
