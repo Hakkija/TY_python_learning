3ea224908771d9cf9d8e4203f2d10c9324183b9d
Hakkija <Henri.tallinn@gmail.com>
2023-03-19 12:35:10 +0200

    good night work >D

--- Project/main.py
+++ Project/main.py
@@ -1,7 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template, request
+import subprocess
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return "Congratulations, it's a web app!"
+    if request.method == 'POST':
+        server_name = request.form['server_name']
+        admin_name = request.form['admin_name']
+        admin_password = request.form['admin_password']
+        server_ip = request.form['server_ip']
+        user_name = request.form['user_name']
+        user_password = request.form['user_password']
+
+        cmd = f"ansible-playbook -i {server_ip}, playbook.yml --extra-vars 'admin_name={admin_name} admin_password={admin_password} user_name={user_name} user_password={user_password}'"
+
+        result = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+
+        if result.returncode != 0:
+            return render_template('index.html', error=result.stderr.decode())
+        else:
+            return render_template('index.html', message="Successfully provisioned server!")
+    else:
+        return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
