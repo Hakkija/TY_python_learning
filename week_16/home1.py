a036fb2e9291a16496514d3ec3710a4747387c43
Hakkija <Henri.tallinn@gmail.com>
2023-06-02 11:28:35 +0300

    new

--- week_16/home1.py
+++ week_16/home1.py
@@ -1,17 +1,28 @@
 import re
 
-def extract_usernames():
+def extract_usernames(filename):
     """
-    This function extracts usernames from University of Tartu's home page URLs found in 'addresses.txt' file.
+    This function extracts usernames from University of Tartu's home page URLs found in the given file.
     Each URL is of the format 'http(s)://www.ut.ee/~username/', where 'username' is the username to be extracted.
-    The function prints each username on a separate line.
+    The function returns a list of usernames.
+    
+    Parameters:
+    filename (str): Name of the text file to be processed.
+
+    Returns:
+    list: A list of extracted usernames.
     """
-    with open('addresses.txt', 'r') as file:
+    usernames = []
+
+    with open(filename, 'r') as file:
         for line in file:
             match = re.search(r'http(s)?://www\.ut\.ee/~([^/]+)/', line)
             if match:
-                print(match.group(2))
+                usernames.append(match.group(2))
+
+    return usernames
 
 
-# Call the function
-extract_usernames('addresses.txt')
+usernames = extract_usernames('addresses.txt')
+for username in usernames:
+    print(username)
